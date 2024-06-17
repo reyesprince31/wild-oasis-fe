5c2f8be3d254641d3ceb9b2431abeb3e10bf7aac
reyesprince31 <reyes.prince31@gmail.com>
2024-06-17 21:55:10 +0800

    feat: supabase link

--- lib/data-service.ts
+++ lib/data-service.ts
@@ -1,5 +1,5 @@
 import { eachDayOfInterval } from 'date-fns';
-
+import { supabase } from './supabase';
 /////////////
 // GET
 
@@ -39,6 +39,9 @@ export const getCabins = async function () {
     .from('cabins')
     .select('id, name, maxCapacity, regularPrice, discount, image')
     .order('name');
+
+  // For testing
+  // await new Promise((res) => setTimeout(res, 2000));
 
   if (error) {
     console.error(error);

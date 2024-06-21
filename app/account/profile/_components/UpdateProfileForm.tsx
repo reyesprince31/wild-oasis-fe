ed35b9818d5220e864ca893e235b942d99f647d8
reyesprince31 <reyes.prince31@gmail.com>
2024-06-21 09:19:24 +0800

    feat: form module that accept props for UpdateProfileForm as csr

---
+++ app/account/profile/_components/UpdateProfileForm.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+export default function UpdateProfileForm({ SelectCountry }: any) {
+  const countryFlag = "pt.jpg";
+  return (
+    <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+      <div className="space-y-2">
+        <label>Full name</label>
+        <input
+          disabled
+          className="px-5 py-3 bg-primary text-primary w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <label>Email address</label>
+        <input
+          disabled
+          className="px-5 py-3 bg-primary text-primary w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
+        />
+      </div>
+
+      <div className="space-y-2">
+        <div className="flex items-center justify-between">
+          <label htmlFor="nationality">Where are you from?</label>
+          <img
+            src={countryFlag}
+            alt="Country flag"
+            className="h-5 rounded-sm"
+          />
+        </div>
+        {SelectCountry}
+      </div>
+
+      <div className="space-y-2">
+        <label htmlFor="nationalID">National ID number</label>
+        <input
+          name="nationalID"
+          className="px-5 py-3 bg-secondary text-primary w-full shadow-sm rounded-sm"
+        />
+      </div>
+
+      <div className="flex justify-end items-center gap-6">
+        <button className="bg-accent px-8 py-4 text-primary font-semibold hover:bg-primary hover:text-secondary transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
+          Update profile
+        </button>
+      </div>
+    </form>
+  );
+}

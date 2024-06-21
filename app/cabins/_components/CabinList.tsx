3deb56bd813f40603faaea248d6896ae3fe0b521
reyesprince31 <reyes.prince31@gmail.com>
2024-06-21 08:40:30 +0800

    feat: filter logic for CabinList

--- app/cabins/_components/CabinList.tsx
+++ app/cabins/_components/CabinList.tsx
@@ -1,16 +1,38 @@
 import { unstable_noStore } from "next/cache";
 import CabinCard from "./CabinCard";
 import { getCabins } from "@/lib/data-service";
+import { FilterOption } from "../page";
 
-async function CabinList() {
+type Cabin = {
+  id: string;
+  name: string;
+  maxCapacity: number;
+  regularPrice: number;
+  discount: number;
+  image: string;
+};
+
+async function CabinList({ filter }: { filter: FilterOption }) {
   //remove cache
-  unstable_noStore();
-  const cabins = await getCabins();
+  // unstable_noStore();
+  const cabins: Cabin[] = await getCabins();
+
+  let displayedCabins: Cabin[] = [];
+  if (filter === "all") displayedCabins = cabins;
+  if (filter === "small")
+    displayedCabins = cabins.filter((c) => c.maxCapacity <= 3);
+  if (filter === "medium")
+    displayedCabins = cabins.filter(
+      (c) => c.maxCapacity >= 4 && c.maxCapacity <= 7
+    );
+  if (filter === "large")
+    displayedCabins = cabins.filter((c) => c.maxCapacity >= 8);
 
   if (!cabins.length) return null;
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayedCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>

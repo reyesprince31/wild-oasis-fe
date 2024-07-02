44979c4e648f322e641c93a08e8e0c801c6e0401
reyesprince31 <reyes.prince31@gmail.com>
2024-07-02 08:42:16 +0800

    chore: update SignOutButton.tsx component

--- app/account/_components/SignOutButton.tsx
+++ app/account/_components/SignOutButton.tsx
@@ -1,11 +1,14 @@
+import { signOutAction } from "@/lib/action";
 import { LogOut } from "lucide-react";
 
 function SignOutButton() {
   return (
-    <button className="py-3 px-5 hover:bg-primary hover:text-secondary transition-colors flex items-center gap-4 font-semibold text-primary w-full">
-      <LogOut className="h-5 w-5 text-primary" />
-      <span>Sign out</span>
-    </button>
+    <form action={signOutAction}>
+      <button className="py-3 px-5 hover:bg-primary hover:text-secondary transition-colors flex items-center gap-4 font-semibold text-primary w-full">
+        <LogOut className="h-5 w-5 text-primary" />
+        <span>Sign out</span>
+      </button>
+    </form>
   );
 }
 

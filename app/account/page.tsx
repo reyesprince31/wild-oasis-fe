bab968a876525b1d203b326ad19e3a741747332c
reyesprince31 <reyes.prince31@gmail.com>
2024-07-02 08:41:44 +0800

    chore: Update account page.tsx

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,9 +1,18 @@
+import { auth } from "@/lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-function page() {
-  return <div>page</div>;
+async function page() {
+  const session = await auth();
+  const firstName = session!.user!.name!.split(" ").at(0);
+
+  return (
+    <h2 className="front-semibold text-2xl text-accent">
+      Welcome, {firstName}
+    </h2>
+  );
 }
 
 export default page;

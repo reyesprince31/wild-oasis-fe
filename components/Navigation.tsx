3509c82922251ecb044c2a2eefba79311eafa453
reyesprince31 <reyes.prince31@gmail.com>
2024-07-02 08:37:48 +0800

    feat: Navigation.tsx Component and render user avatar if logged in

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,6 +1,9 @@
+import { auth } from "@/lib/auth";
 import Link from "next/link";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
+
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -15,9 +18,25 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link href="/account" className="hover:text-accent transition-colors">
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="hover:text-accent transition-colors flex item-center gap-4">
+              <img
+                src={session.user.image}
+                alt={session.user.name}
+                className="h-8 rounded-full"
+                referrerPolicy="no-referrer"
+              />
+              <span>Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent transition-colors">
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>

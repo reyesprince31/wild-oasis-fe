6fbd91a69346a804172cedae4c5649f11cbd756c
reyesprince31 <reyes.prince31@gmail.com>
2024-07-02 08:40:00 +0800

    refactor: Reservation.tsx component to include a session if logged in

--- app/cabins/[cabinId]/_components/Reservation.tsx
+++ app/cabins/[cabinId]/_components/Reservation.tsx
@@ -1,12 +1,16 @@
 import { getSettings, getBookedDatesByCabinId } from "@/lib/data-service";
 import DateSelector from "./DateSelector";
 import ReservationForm from "./ReservationForm";
+import LoginMessage from "@/components/LoginMessage";
+import { auth } from "@/lib/auth";
 
 async function Reservation({ cabin }: any) {
   const [settings, bookedDates] = await Promise.all([
     getSettings(),
     getBookedDatesByCabinId(cabin.id),
   ]);
+
+  const session = await auth();
   return (
     <div className="grid grid-cols-2 border border-primary min-h-[400px]">
       <DateSelector
@@ -14,7 +18,11 @@ async function Reservation({ cabin }: any) {
         bookedDates={bookedDates}
         cabin={cabin}
       />
-      <ReservationForm cabin={cabin} />
+      {session?.user ? (
+        <ReservationForm cabin={cabin} user={session.user} />
+      ) : (
+        <LoginMessage />
+      )}
     </div>
   );
 }
